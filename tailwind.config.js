eddc528c9f9c4121592e22592bcf6e255496fecc
colficol-jpg <colficol@gmail.com>
2025-10-27 14:05:36 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: { lgRed: "#A50034" },
+    },
+  },
+  plugins: [],
+};
