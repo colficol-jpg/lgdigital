a650319c7e73d8198433132c4194e1a10e3d7b84
colficol-jpg <colficol@gmail.com>
2025-10-27 17:09:32 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,12 @@ export default function Document() {
   return (
     <Html lang="es">
       <Head>
-        {/* Fuente LG (Noto Sans) */}
+        {/* Fuente LG */}
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
-        {/* Iconos Font Awesome */}
+        {/* Iconos */}
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
