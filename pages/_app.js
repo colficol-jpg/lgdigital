41cff542c5fb66e3ebe1b55d6b0032ab13cb47c0
colficol-jpg <colficol@gmail.com>
2025-10-27 17:14:33 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 // pages/_app.js
 import "@/styles/globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }) {
       <main className="min-h-screen bg-white">
         <Component {...pageProps} />
       </main>
+      <Footer />
     </>
   );
 }
