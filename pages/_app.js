50b8fe6ec615ea2773860b209877cc4e31608f41
colficol-jpg <colficol@gmail.com>
2025-10-27 17:02:42 -0500

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+// pages/_app.js
+import "@/styles/globals.css";
+import Header from "@/components/Header";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen bg-white">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
+}
